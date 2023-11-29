93377bd1fd8103455cef350df3abfe81ac9fec87
poole <poole@endeavor.cs.widener.edu>
2023-11-29 13:12:17 -0500

    messages

--- src/textGame/Bottle.java
+++ src/textGame/Bottle.java
@@ -6,12 +6,25 @@ public class Bottle extends Item{
 		super ("bottle");
 	}
 	
+	public void take() {
+		super.take();
+		switch(game.getCurrentRoom().getRoomID()) {
+		case "LOBBY":
+			game.getCurrentRoom().setRoomID("LOBBY_A");
+			break;
+		case "LOBBY_B":
+			game.getCurrentRoom().setRoomID("LOBBY_C");
+			break;
+		}
+	}
+	
 	public void use() {
 		if(isUsed()) {
 			game.print("An empty bottle that used to be full of soda.");
 		} else {
 			game.print("You chug the bottle of room temperature soda. You feel no effect besides your thirst being quenched.");
 			setUsed(true);
+			
 		}
 	}
 }
